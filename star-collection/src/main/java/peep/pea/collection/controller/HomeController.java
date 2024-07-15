bd3ceef2e1382880e2c8884aa33d63085f8b0525
Drew <D.Murphy.Erskine@gmail.com>
2024-07-15 16:04:38 -0400

    active nav

--- star-collection/src/main/java/peep/pea/collection/controller/HomeController.java
+++ star-collection/src/main/java/peep/pea/collection/controller/HomeController.java
@@ -3,7 +3,10 @@ package peep.pea.collection.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 
@@ -23,5 +26,10 @@ public class HomeController {
     @GetMapping("/aboutPeepPea")
     public String aboutPeepPea(Model model) {
         return "about-peeppea";
+    }
+    
+    @ModelAttribute("currentURI")
+    public String getCurrentURI(HttpServletRequest request) {
+        return request.getRequestURI();
     }
 }
